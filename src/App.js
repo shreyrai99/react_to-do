a564f3cc66543b6da6e6c28af50196089eab6096
Shrey Rai <kumarshrey.21099@gmail.com>
2020-10-31 14:40:38 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Todos from './components/Todos';
 import './App.css';
+import Header from './components/layout/Header';
+import { AddTodo } from './AddTodo';
 
-function App() {
+
+class App extends Component{
+
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out Trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner time tommy',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'I love Tommy',
+        completed: false
+      }
+    ]
+  }
+
+
+  markComplete = (id)=>{
+    this.setState({todos: this.state.todos.map(todo=>{
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+  }
+
+  //delete todo
+  delTodo = (id) =>{
+    this.setState({todos: [...this.state.todos.filter(todo=>todo.id!==id)]});
+  }
+
+  //add Todo
+  addTodo = (title)=>{
+    const newTodo = {
+      id: Math.floor(Math.random() * 1000),
+      title,
+      completed: false
+    }
+    this.setState({todos: [...this.state.todos, newTodo]});
+  }
+  render() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+
+      <Header />
+      <AddTodo addTodo={this.addTodo}/>
+      <Todos todos={this.state.todos} markComplete={this.markComplete}
+      delTodo={this.delTodo}/>
+      </div>
     </div>
   );
 }
-
+}
 export default App;
